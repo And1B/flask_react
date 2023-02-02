02ba83689b5c486678cad6b3d425189a6092aef7
Andi <andi.buhn@gmail.com>
2023-02-02 07:28:52 +0100

    flask-react connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useState } from 'react'
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+   // new line start
+  const [profileData, setProfileData] = useState(null)
+
+  function getData() {
+    axios({
+      method: "GET",
+      url:"/profile",
+    })
+    .then((response) => {
+      const res = response.data;
+      setProfileData(({
+        name: res.name,
+        about: res.about}))
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+        }
+    })}
+    //end of new line 
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +43,19 @@ function App() {
         >
           Learn React
         </a>
+
+        {/* new line start*/}
+        <p>To get your profile details: </p><button onClick={getData}>Click me</button>
+        {profileData && <div>
+              <p>Profile name: {profileData.name}</p>
+              <p>About me: {profileData.about}</p>
+            </div>
+        }
+         {/* end of new line */}
       </header>
     </div>
   );
 }
 
 export default App;
+
